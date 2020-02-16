f9a4589dd132a4f58a0196ea90bfad6fbbdb5a64
fredsmoot <61094148+fredsmoot@users.noreply.github.com>
2020-02-16 15:55:08 -0500

    Update AppReducer.kt

--- redux/AppReducer.kt
+++ redux/AppReducer.kt
@@ -1,6 +1,6 @@
 // Add top-level appReducer so that its called when an action is dispatched.
 
 data class appReducer(state: AppState, action: Any) = when(action) {
-coreAppState = reduceCoreAppState(state.coreAppState, action),
-counterScreenState = reduceCounterScreenState(state.counterScreenState, action)
+  coreAppState = reduceCoreAppState(state.coreAppState, action),
+  counterScreenState = reduceCounterScreenState(state.counterScreenState, action)
 }
