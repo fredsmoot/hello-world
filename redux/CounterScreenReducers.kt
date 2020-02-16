5dfa4aed41c421f2e30766584cfdda9e3de0ea6b
fredsmoot <61094148+fredsmoot@users.noreply.github.com>
2020-02-16 14:51:27 -0500

    Update CounterScreenReducers.kt

--- redux/CounterScreenReducers.kt
+++ redux/CounterScreenReducers.kt
@@ -1,1 +1,9 @@
+// Reducers are pure functions where app state can be copied and set.
+// The state is checked by a when conditional on a certain Action.
 
+fun reducerCounterScreenState(state: CounterScreenState, action: Any) = when(action) {
+  is ToCounterScreen -> CounterScreenState() // reinitialize state.
+  is Increment -> state.copy(count = state.count + 1)
+  is Decrement -> state.copy(count = state.count - 1)
+  else -> state
+}
