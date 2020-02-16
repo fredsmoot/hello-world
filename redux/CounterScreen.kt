9b094b18231296e587be46d0778af085693e2f82
fredsmoot <61094148+fredsmoot@users.noreply.github.com>
2020-02-16 14:46:20 -0500

    Update CounterScreen.kt

--- redux/CounterScreen.kt
+++ redux/CounterScreen.kt
@@ -1,1 +1,10 @@
 
+// Navigation view management. We use Flow library where each view is an 
+// object that requires a key as its indentifier.
+
+object CounterScreen: Screen() {
+  val layout = R.layout.screen_counter
+  
+  val navigation = Navigation.NORTH_SOUTH
+}
+
