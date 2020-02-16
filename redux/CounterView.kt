b5a45fe684404b5f2a03c20773144d654a1d59a0
DerrickHenry <61090611+DerrickHenry@users.noreply.github.com>
2020-02-16 16:13:29 -0500

    Update CounterView.kt

--- redux/CounterView.kt
+++ redux/CounterView.kt
@@ -1,1 +1,16 @@
+// Holds the state
+data class CounterViewModel(val count: Int) {
+  override toString(): String {
+    return "CounterViewModel(count = $count)"
+  }
+}
 
+// Transformational function converting state to viewmodel 
+// so we can render values to our views.
+fun toCounterViewModel(state: AppState) = CounterViewModel(
+  count = state.counterAppState.count.toString()
+)
+
+fun render(model: CounterStateModel) {
+  countView.text = model.count
+}
